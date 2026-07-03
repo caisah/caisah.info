0b0ae4a28205e993fc15a44d883da34f291bc6ab
caisah <vlad.piersec@protonmail.com>
2026-07-03 16:16:27 +0300

    Fix sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
+import { copyFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
@@ -8,7 +11,17 @@ export default defineConfig({
     site: 'https://caisah.info',
     integrations: [
         mdx(),
-        sitemap()
+        sitemap(),
+        {
+            name: 'conventional-sitemap-url',
+            hooks: {
+                'astro:build:done': async ({ dir, logger }) => {
+                    const outputDirectory = fileURLToPath(dir);
+                    await copyFile(join(outputDirectory, 'sitemap-index.xml'), join(outputDirectory, 'sitemap.xml'));
+                    logger.info('`sitemap.xml` copied from `sitemap-index.xml`');
+                }
+            }
+        }
     ],
     vite: {
         plugins: [tailwindcss()]
